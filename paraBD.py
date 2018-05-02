a0b0f62ea52eac2bdf20892df383cbd8e1f1262e
Francisco Rosal <francisco-rosal98@hotmail.com>
2018-05-02 11:29:18 -0600

    Para Base de Datos

---
+++ paraBD.py
@@ -0,0 +1,29 @@
+lugar = {
+		'Departamento': "Sacatepequez",
+		'Categoria': "Restaurante",
+		'Nombre': "Angie Angie",
+		'Direccion': "Aqui ponen la direccion",
+		'Telefono': 5555 5555,
+		'Web': "Aqui ponen la pagina web si tiene, sino solo las comillas",
+		'Horario': "Horario si lo encuentran",
+		'Descripcion': "Sirven pizzas y comida rica",
+		'Comentarios':[{
+			'Comentario': "",
+			'Puntuacion': ""
+		}]
+	}
+
+lugar = {
+		'Departamento': "Sacatepequez",
+		'Categoria': "Restaurante",
+		'Nombre': "Angie Angie",
+		'Direccion': "Aqui ponen la direccion",
+		'Telefono': 5555 5555,
+		'Web': "Aqui ponen la pagina web si tiene, sino solo las comillas",
+		'Horario': "Horario si lo encuentran",
+		'Descripcion': "Sirven pizzas y comida rica",
+		'Comentarios':[{
+			'Comentario': "",
+			'Puntuacion': ""
+		}]
+	}
