b43d5da9b4544dfb6ed27a4ca617f194129eb134
Francisco Rosal <francisco-rosal98@hotmail.com>
2018-05-17 16:10:06 -0600

    BD parte 1

--- paraBD.py
+++ paraBD.py
@@ -1,29 +1,483 @@
 lugar = {
-		'Departamento': "Sacatepequez",
+		'Departamento': "Sololá",
 		'Categoria': "Restaurante",
-		'Nombre': "Angie Angie",
-		'Direccion': "Aqui ponen la direccion",
-		'Telefono': 5555 5555,
-		'Web': "Aqui ponen la pagina web si tiene, sino solo las comillas",
-		'Horario': "Horario si lo encuentran",
-		'Descripcion': "Sirven pizzas y comida rica",
+		'Nombre': "Café loco",
+		'Direccion': "Calle Santander Panajachel, Sololá",
+		'Telefono': 47043588,
+		'Web': "",
+		'Horario': "9:00 a.m. a 8:00 p.m.",
+		'Descripcion': "Ofrecen bebidas con café que son deliciosas y que muestran variedad y mucho sabor. Cada semana ofrecen una nueva propuesta de bebida, en Café Loco nunca probarás dos veces el mismo sabor.",
 		'Comentarios':[{
-			'Comentario': "",
-			'Puntuacion': ""
+			'Comentario': "Café de alta calidad|Muchas opciones de bebidas frías y calientes",
+			'Puntuacion': 5
 		}]
 	}
 
 lugar = {
-		'Departamento': "Sacatepequez",
+		'Departamento': "Sololá",
 		'Categoria': "Restaurante",
-		'Nombre': "Angie Angie",
-		'Direccion': "Aqui ponen la direccion",
-		'Telefono': 5555 5555,
-		'Web': "Aqui ponen la pagina web si tiene, sino solo las comillas",
-		'Horario': "Horario si lo encuentran",
-		'Descripcion': "Sirven pizzas y comida rica",
+		'Nombre': "Guajimbo's",
+		'Direccion': "Santander, panajachel Guatemala",
+		'Telefono': 77620063,
+		'Web': "",
+		'Horario': "",
+		'Descripcion': "Churrasqueria sudamericana y centroamericana",
 		'Comentarios':[{
-			'Comentario': "",
-			'Puntuacion': ""
+			'Comentario': "Buena comida al estilo uruguayo",
+			'Puntuacion': 4
+		}]
+	}
+
+lugar = {
+		'Departamento': "Sololá",
+		'Categoria': "Recreativo",
+		'Nombre': "Mirador Rostro Maya",
+		'Direccion': "San Juan la laguna, Sololá",
+		'Telefono': "",
+		'Web': "",
+		'Horario': "",
+		'Descripcion': "Hermoso mirador de San Juan la laguna.",
+		'Comentarios':[{
+			'Comentario': "Panorama de 360 grados, vista espectacular.",
+			'Puntuacion': 5
+		}]
+	}
+
+lugar = {
+		'Departamento': "Sololá",
+		'Categoria': "Recreativo",
+		'Nombre': "Museo Lacustre",
+		'Direccion': "En las instalaciones del Hotel La osada de Don Rodrigo, final de la calle Santander",
+		'Telefono': 7762 2326,
+		'Web': "",
+		'Horario': "Lunes a Sábado 8:00 a.m. a 3:00 p.m.",
+		'Descripcion': "Este museo se formó con las piezas maas encontradas bajo el agua, en la ciudad que existía en Atitlán",
+		'Comentarios':[{
+			'Comentario': "Ciudad perdida, Exploración maya, Hermoso museo.",
+			'Puntuacion': 4
+		}]
+	}
+
+lugar = {
+		'Departamento': "Izabal",
+		'Categoria': "Restaurante",
+		'Nombre': "Ranchon Mary",
+		'Direccion': "CA-13, Río Dulce",
+		'Telefono': 79305103,
+		'Web': "",
+		'Horario': "Todos los días 6:30 a.m. a 8:30 p.m.",
+		'Descripcion': "Restauante de mariscos",
+		'Comentarios':[{
+			'Comentario': "Las mejores mojarras del área",
+			'Puntuacion': 4
+		}]
+	}
+
+lugar = {
+		'Departamento': "Izabal",
+		'Categoria': "Restaurante",
+		'Nombre': "Happy Fish",
+		'Direccion':" Livingston, Izabal, Calle principal de comercio",
+		'Telefono': 79470661,
+		'Web': "",
+		'Horario': "Todos los días 7:00 a.m. a 10:00 p.m.",
+		'Descripcion': "Restauante de mariscos",
+		'Comentarios':[{
+			'Comentario': "Apto para niños",
+			'Puntuacion': 4
+		}]
+	}
+
+lugar = {
+		'Departamento': "Izabal",
+		'Categoria': "Recreación",
+		'Nombre': "El boqueron",
+		'Direccion':" El estor, Izabal",
+		'Telefono': "",
+		'Web': "",
+		'Horario': "8:00 a.m. a 5:00 p.m.",
+		'Descripcion': "Paseo por las aguas",
+		'Comentarios':[{
+			'Comentario': "Recorrido genial en cayuco",
+			'Puntuacion': 5
+		}]
+	}
+
+lugar = {
+		'Departamento': "Izabal",
+		'Categoria': "Recreación",
+		'Nombre': "Casa Gandau",
+		'Direccion':" Río Dulce, Izabal",
+		'Telefono': 55113280,
+		'Web': "",
+		'Horario': "8:00 a.m. a 4:00 p.m.",
+		'Descripcion': "Relajarte en medio de la naturaleza",
+		'Comentarios':[{
+			'Comentario': "Casa lujosa",
+			'Puntuacion': 4
+		}]
+	}
+
+lugar = {
+		'Departamento': "Jutiapa",
+		'Categoria': "Restaurante",
+		'Nombre': "Drock's seafood & steak restaurant",
+		'Direccion':" Jutiapa",
+		'Telefono': 78467869,
+		'Web': "",
+		'Horario': "7:00 a.m. a 9:00 p.m.",
+		'Descripcion': "Disfruta de Mariscos y carnes",
+		'Comentarios':[{
+			'Comentario': "un hermosos lugar",
+			'Puntuacion': 4
+		}]
+	}
+
+lugar = {
+		'Departamento': "Jutiapa",
+		'Categoria': "Restaurante",
+		'Nombre': "pepepanes",
+		'Direccion':" calle 6 de septiembre 1-17,Jutiapa 22000",
+		'Telefono': 78441088,
+		'Web': "",
+		'Horario': "11:00 a.m. a 8:00 p.m.",
+		'Descripcion': "Comida rapida jutiapaneca",
+		'Comentarios':[{
+			'Comentario': "ideal para niños",
+			'Puntuacion': 4
+		}]
+	}
+
+lugar = {
+		'Departamento': "Jutiapa",
+		'Categoria': "Recreación",
+		'Nombre': "Finca D'Rocks",
+		'Direccion':" Aldea Las Flores, El Progreso, Jutiapa",
+		'Telefono': 78234906,
+		'Web': "",
+		'Horario': "7:00 a.m. a 9:00 p.m.",
+		'Descripcion': "piscina con vista a volcanes",
+		'Comentarios':[{
+			'Comentario': "un hermosos lugar con instalaciones rusticas",
+			'Puntuacion': 5
+		}]
+	}
+
+lugar = {
+		'Departamento': "Jutiapa",
+		'Categoria': "Recreación",
+		'Nombre': "Eco Parque Mongoy",
+		'Direccion':" Kilómetro 156 Jutiapa",
+		'Telefono': 53755410,
+		'Web': "",
+		'Horario': "10:00 a.m. a 5:00 p.m.",
+		'Descripcion': "Piscinas al lado del río",
+		'Comentarios':[{
+			'Comentario': "Ideal para excursiones",
+			'Puntuacion': 4
+		}]
+	}
+
+
+lugar = {
+		'Departamento': "suchitepéquez",
+		'Categoria': "Restaurante",
+		'Nombre': "Restaurante Casa Vieja",
+		'Direccion':" 7 avenida calle 3-46 zona 1.",
+		'Telefono': 53105499,
+		'Web': "",
+		'Horario': "7:00 a.m. a 5:30 p.m.",
+		'Descripcion': "Comida excelente",
+		'Comentarios':[{
+			'Comentario': "para compartir en familia",
+			'Puntuacion': 4
+		}]
+	}
+
+lugar = {
+		'Departamento': "suchitepéquez",
+		'Categoria': "Restaurante",
+		'Nombre': "El Rancho",
+		'Direccion':" km 113 CA2 interio estación shell cocales, patulul, suchitepéquez",
+		'Telefono': 78719796,
+		'Web': "",
+		'Horario': "7:00 a.m. a 9:00 p.m.",
+		'Descripcion': "Comida del mar",
+		'Comentarios':[{
+			'Comentario': "Su especialidad la tilapi",
+			'Puntuacion': 4
+		}]
+	}
+
+lugar = {
+		'Departamento': "Suchitepéquez",
+		'Categoria': "Recreación",
+		'Nombre': "El Chorro del carmen",
+		'Direccion':" km164 desde la capital, Zunilito, suchitepequez 01018",
+		'Telefono': 42107370,
+		'Web': "",
+		'Horario': "9:30 a.m. a 5:00 p.m.",
+		'Descripcion': "piscinas",
+		'Comentarios':[{
+			'Comentario': "buen lugar para contratar lancha privada",
+			'Puntuacion': 5
+		}]
+	}
+
+lugar = {
+		'Departamento': "Suchitepéquez",
+		'Categoria': "Recreación",
+		'Nombre': "Parque Ecológico Cristiano La Merced",
+		'Direccion':" Jutiapa",
+		'Telefono': 57805821,
+		'Web': "",
+		'Horario': "10:00 a.m. a 4:00 p.m.",
+		'Descripcion': "Para disfrutar con mi familia",
+		'Comentarios':[{
+			'Comentario': "elegante, muy bonito y divertido",
+			'Puntuacion': 4
+		}]
+	}
+
+lugar = {
+		'Departamento': "Zacapa",
+		'Categoria': "Restaurante",
+		'Nombre': "El Establo",
+		'Direccion':" Zacapa",
+		'Telefono': 79412709,
+		'Web': "",
+		'Horario': "6:00 a.m. a 10:00 p.m.",
+		'Descripcion': "Variedad de comida",
+		'Comentarios':[{
+			'Comentario': "ambiente agradable",
+			'Puntuacion': 4
+		}]
+	}
+
+lugar = {
+		'Departamento': "Zacapa",
+		'Categoria': "Restaurante",
+		'Nombre': "Los Chevys WJB",
+		'Direccion':"Zacapa",
+		'Telefono': 79410121,
+		'Web': "",
+		'Horario': "9:00 a.m. a 10:00 p.m.",
+		'Descripcion': "Deliciosas Tortillas De Harina",
+		'Comentarios':[{
+			'Comentario': "exquisito sabor",
+			'Puntuacion': 4
+		}]
+	}
+
+lugar = {
+		'Departamento': "Zacapa",
+		'Categoria': "Recreación",
+		'Nombre': "Valle Dorado",
+		'Direccion':"KM 149, carr Jacobo Árbenz Guzmán",
+		'Telefono': 42107370,
+		'Web': "",
+		'Horario': "9:30 a.m. a 5:00 p.m.",
+		'Descripcion': "Parque acuático y recrativo",
+		'Comentarios':[{
+			'Comentario': "Bellas instalaciones",
+			'Puntuacion': 5
+		}]
+	}
+
+lugar = {
+		'Departamento': "Zacapa",
+		'Categoria': "Recreación",
+		'Nombre': "Museo de Paleontologia Estanzuela",
+		'Direccion':" 5 Avenida, Estanzuela",
+		'Telefono': 79336108,
+		'Web': "",
+		'Horario': "8:00 a.m. a 5:00 p.m.",
+		'Descripcion': "Museo de historia natural",
+		'Comentarios':[{
+			'Comentario': "No tiene costo de ingreso",
+			'Puntuacion': 4
+		}]
+	}
+
+
+lugar = {
+		'Departamento': "Sacatepéquez",
+		'Categoria': "Restaurante",
+		'Nombre': "Fridas",
+		'Direccion':" 5a Avenida Norte #29, Arco de Santa Calina, Antigua Guatemala 03001",
+		'Telefono': 78321296,
+		'Web': "",
+		'Horario': "12:00 p.m. a 1:00 a.m.",
+		'Descripcion': "Variedad de comida mexicana",
+		'Comentarios':[{
+			'Comentario': "Excelente atención",
+			'Puntuacion': 4
+		}]
+	}
+
+lugar = {
+		'Departamento': "Sacatepéquez",
+		'Categoria': "Restaurante",
+		'Nombre': "Nim Guaa",
+		'Direccion':"carr. a San Lucas Sacatepéquez",
+		'Telefono': 78303174,
+		'Web': "",
+		'Horario': "9:00 a.m. a 10:00 p.m.",
+		'Descripcion': "Deliciosa comida nacional",
+		'Comentarios':[{
+			'Comentario': "Muy bonito lugar",
+			'Puntuacion': 4
+		}]
+	}
+
+lugar = {
+		'Departamento': "Sacatepéquez",
+		'Categoria': "Recreación",
+		'Nombre': "Choco Museo",
+		'Direccion':"4a calle Oriente, Antigua Guatemala",
+		'Telefono': 78324520,
+		'Web': "",
+		'Horario': "10:30 a.m. a 6:30 p.m.",
+		'Descripcion': "Museo de Chocolate",
+		'Comentarios':[{
+			'Comentario': "Bellas instalaciones, chocolates ricos",
+			'Puntuacion': 5
+		}]
+	}
+
+lugar = {
+		'Departamento': "Sacatepéquez",
+		'Categoria': "Recreación",
+		'Nombre': "Casa Popenoe",
+		'Direccion':" 5a calle Oriente, Antigua Guatemala",
+		'Telefono': 23387959,
+		'Web': "",
+		'Horario': "9:00 a.m. a 4:00 p.m.",
+		'Descripcion': "Centro Cultural",
+		'Comentarios':[{
+			'Comentario': "Residencia Colonial hermosa",
+			'Puntuacion': 5
+		}]
+	}
+
+
+lugar = {
+		'Departamento': "Petén",
+		'Categoria': "Restaurante",
+		'Nombre': "Katok Petén",
+		'Direccion':" petén",
+		'Telefono': 78403387,
+		'Web': "",
+		'Horario': "6:00 a.m. a 10:00 p.m.",
+		'Descripcion': "Variedad de comida nacional",
+		'Comentarios':[{
+			'Comentario': "Decoración maya",
+			'Puntuacion': 4
+		}]
+	}
+
+lugar = {
+		'Departamento': "Petén",
+		'Categoria': "Restaurante",
+		'Nombre': "Legumbres Maya",
+		'Direccion':"Calle Centro América, Flores",
+		'Telefono': 30417735,
+		'Web': "",
+		'Horario': "8:00 a.m. a 9:00 p.m.",
+		'Descripcion': "Restaurante Vegetariano",
+		'Comentarios':[{
+			'Comentario': "exquisito sabor",
+			'Puntuacion': 5
+		}]
+	}
+
+lugar = {
+		'Departamento': "Petén",
+		'Categoria': "Recreación",
+		'Nombre': "ZOO Petencito",
+		'Direccion':"carr. a San Miguel",
+		'Telefono': "",
+		'Web': "",
+		'Horario': "8:00 a.m. a 6:00 p.m.",
+		'Descripcion': "zoológico",
+		'Comentarios':[{
+			'Comentario': "Un lugar hermoso",
+			'Puntuacion': 5
+		}]
+	}
+
+lugar = {
+		'Departamento': "Petén",
+		'Categoria': "Recreación",
+		'Nombre': "Jorge's Rope Swing",
+		'Direccion':" Petén ",
+		'Telefono': 31963196,
+		'Web': "",
+		'Horario': "8:00 a.m. a 5:00 p.m.",
+		'Descripcion': "Baños termales",
+		'Comentarios':[{
+			'Comentario': "Atracción turística bonita",
+			'Puntuacion': 4
+		}]
+	}
+
+
+lugar = {
+		'Departamento': "Chiquimula",
+		'Categoria': "Restaurante",
+		'Nombre': "Pizza Burguer Diner #2",
+		'Direccion':" Esquipulas",
+		'Telefono': 79430888,
+		'Web': "",
+		'Horario': "7:00 a.m. a 10:00 p.m.",
+		'Descripcion': "Variedad de comida",
+		'Comentarios':[{
+			'Comentario': "La mejor pizza de oriente",
+			'Puntuacion': 4
+		}]
+	}
+
+lugar = {
+		'Departamento': "Chiquimula",
+		'Categoria': "Restaurante",
+		'Nombre': "La Casa de Papa Tono y Mama Lola",
+		'Direccion':"KM 175 carretera a Ipala, Los laureles, Sabana Grande, Chiquimula",
+		'Telefono': 51449964,
+		'Web': "",
+		'Horario': "10:00 a.m. a 9:00 p.m.",
+		'Descripcion': "Restaurante de Carnes Asadas",
+		'Comentarios':[{
+			'Comentario': "exquisito sabor, ideal para niños",
+			'Puntuacion': 4
+		}]
+	}
+
+lugar = {
+		'Departamento': "Chiquimula",
+		'Categoria': "Recreación",
+		'Nombre': "Parque Chatun",
+		'Direccion':"KM 226 carr a Honduras, Esquipulas 20007",
+		'Telefono': 78730909,
+		'Web': "",
+		'Horario': "9:30 a.m. a 5:00 p.m.",
+		'Descripcion': "Parque acuático y recrativo",
+		'Comentarios':[{
+			'Comentario': "Bellas piscinas",
+			'Puntuacion': 5
+		}]
+	}
+
+lugar = {
+		'Departamento': "Chiquimula",
+		'Categoria': "Recreación",
+		'Nombre': "Piedra de los compadres",
+		'Direccion':" RN-18 Esquipulas",
+		'Telefono': "",
+		'Web': "",
+		'Horario': "abierto las 24 horas",
+		'Descripcion': "Centro Cultural",
+		'Comentarios':[{
+			'Comentario': "delicioso jugo de caña y paseo por tuc-tuc",
+			'Puntuacion': 4
 		}]
 	}
